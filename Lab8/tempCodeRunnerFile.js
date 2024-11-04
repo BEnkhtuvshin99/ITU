2a3f0f1f9cfcad8a84386ca0d0748ab418c4dc4e
Enkhtuvshin Bat-Ochir <b.enkhtuvshin99@gmail.com>
2024-11-04 23:47:51 +0800

    aldaa zasav

---
+++ Lab8/tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Lab-8</title>
+  <script>
+    var y;
+    var x = 1;
+    var total = 0;
+    while (x < 10) {
+      y = x * x;
+      document.writeln("<p>" + y + "</p>");
+      total += y;
+      ++x;
+    }
+    document.writeln("<p>Niilber:" + total + "</p>");
+  </script>
+</head>
+<body>
+</body>
+</html>
